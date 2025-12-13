d39400e7fb59a911fdbd63e049af27f84a0db0c0
Bergami <wander.bergami@gmail.com>
2025-12-13 20:06:04 -0300

    add `compose`config file with database service

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,7 @@
+test("Get to /api/v1/status should return status ok", async () => {
+  const res = await fetch("http://localhost:3000/api/v1/status");
+  expect(res.status).toBe(200);
+
+  const data = await res.json();
+  expect(data).toEqual({ status: "ok" });
+});
